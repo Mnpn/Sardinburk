49e10b5bb4becd71144a63f04dbc6e0ac4be9178
Martin Persson <mnpn03@gmail.com>
2018-02-18 20:59:25 +0100

    Progress, I guess.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 extern crate clap;
 use clap::{App, Arg};
-use std::io::{Error, Read};
-use std::fs::File;
-use std::fs::OpenOptions; 
+use std::io::{Error};
+use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream, SocketAddr};
 
 fn main() {
     // If any error would occur in inner_main(), print the error.
@@ -20,7 +20,7 @@ fn inner_main() -> Result<(), Error> {
         .about(crate_description!())
         .author(crate_authors!())
         .arg(Arg::with_name("ip")
-            .help("The IP and port to connect to.") // Not sure if this is how we're going to do this, just a clap placeholder.
+            .help("The IP to connect to.") // Not sure if this is how we're going to do this, just a clap placeholder.
             .required(true) // Make argument required.
             .index(1))
         .get_matches();
@@ -28,6 +28,13 @@ fn inner_main() -> Result<(), Error> {
     // Define variables.
     // Split IP and Port TBD.
     let ip = matches.value_of("ip").unwrap();
+    // Create a TcpListener.
+    // Use port 2037 if port 2580 fails.
+    let addrs = [
+        SocketAddr::from(([127, 0, 0, 1], 2580)),
+        SocketAddr::from(([127, 0, 0, 1], 2037)),
+    ];
+    let listener = TcpListener::bind(&addrs[..]).unwrap();
 
     let mut file = OpenOptions::new()
         .append(true)
@@ -39,8 +46,18 @@ fn inner_main() -> Result<(), Error> {
         eprintln!("Couldn't write to file: {}", e);
     }
 
+    // Accept connections.
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
     // Temporary test print.
     println!("{}", ip);
     // Everything completed without any fatal issues! Well done, code!
     Ok(())
+}
+
+// Handle incoming TCP connections.
+fn handle_client(stream: TcpStream) {
+    println!("Wow, I got something!");
 }
