ff124a1d60557806c5fcd6ea50c8eeed682abf25
Martin Persson <mnpn03@gmail.com>
2018-02-19 19:07:14 +0100

    Client progress!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 #[macro_use]
 extern crate clap;
+
 use clap::{App, Arg};
-use std::io::{Error};
 use std::fs::{OpenOptions, File};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::io;
+use serde_json::Error as SerdeError;
+use std::io::Error;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    user_id: i8,
+    message: String,
+}
 
 fn main() {
     // If any error would occur in inner_main(), print the error.
@@ -34,17 +47,38 @@ fn inner_main() -> Result<(), Error> {
         .unwrap();
 
     if let Some(ip) = matches.value_of("ip") { // If IP argument exists
-    // Assume they want to connect to another instance. [Client]
-    let addrs = [
-        SocketAddr::from(([0, 0, 0, 0], 2580)),
-        SocketAddr::from(([0, 0, 0, 0], 2037)),
-    ];
-        if let Ok(stream) = TcpStream::connect(&addrs[..]) {
-            println!("Connected!");
-        } else {
-            println!("Couldn't connect.");
+        // Assume they want to connect to another instance. [Client]
+        let user_id = 0; // Client ID is always 0.
+        let addrs = [
+            SocketAddr::from(([0, 0, 0, 0], 2580)),
+            SocketAddr::from(([0, 0, 0, 0], 2037)),
+        ];
+            if let Ok(_stream) = TcpStream::connect(&addrs[..]) {
+                println!("Connected!");
+            } else {
+                println!("Couldn't connect.");
+            }
+
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let line = match line {
+                Ok(line) => {
+                    let message = Message {
+                        user_id: user_id.to_owned(),
+                        message: line,
+                    };
+                    let msg = serde_json::to_string(&message)?;
+
+                    println!("{}", msg);
+                },
+                Err(err) => {
+                    eprintln!("I/O error: {}", err);
+                    continue;
+                },
+            };
         }
     } else { // No IP was supplied. Assuming they want to recieve a connection. [Server]
+        let user_id = 1; // Server ID is always 1.
         // Create a TcpListener.
         // Use port 2037 if port 2580 fails.
         let addrs = [
@@ -55,7 +89,7 @@ fn inner_main() -> Result<(), Error> {
 
         // Accept connections.
         for stream in listener.incoming() {
-            handle_client(&mut file, stream?);
+            handle_client(&mut file, user_id, stream?);
         }
     }
 
@@ -64,9 +98,9 @@ fn inner_main() -> Result<(), Error> {
 }
 
 // Handle incoming TCP connections.
-fn handle_client(logfile: &mut File, stream: TcpStream) {
+fn handle_client(logfile: &mut File, user_id: i8, stream: TcpStream) {
     println!("Wow, I got something!");
-    log(logfile, 1, "yas, conect!");
+    log(logfile, user_id, "Connection was made!");
 }
 
 fn log(logfile: &mut File, id: i8, message: &str) {
