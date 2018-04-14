ad42d4e733eac4046ba7d3cc0e56038d8c9a032e
Martin Persson <mnpn03@gmail.com>
2018-04-14 17:28:46 +0200

    Tabs > Spaces

--- src/main.rs
+++ src/main.rs
@@ -20,147 +20,147 @@ use rustyline::Editor;
 
 #[derive(Serialize, Deserialize)]
 struct Message {
-    user_id: i8,
-    message: String,
+	user_id: i8,
+	message: String,
 }
 
 fn main() {
-    // If any error would occur in inner_main(), print the error.
-    if let Err(err) = inner_main() {
-        eprintln!("{}", err);
-    }
+	// If any error would occur in inner_main(), print the error.
+	if let Err(err) = inner_main() {
+		eprintln!("{}", err);
+	}
 }
 
 fn inner_main() -> Result<(), Error> {
-    // clap app creation, with macros that read project information from Cargo.toml.
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .author(crate_authors!())
-        .arg(Arg::with_name("ip")
-            .help("The IP to connect to.") // Not sure if this is how we're going to do this, just a clap placeholder.
-            .required(false) // Don't make argument required.
-            .index(1))
-        .get_matches();
+	// clap app creation, with macros that read project information from Cargo.toml.
+	let matches = App::new(crate_name!())
+		.version(crate_version!())
+		.about(crate_description!())
+		.author(crate_authors!())
+		.arg(Arg::with_name("ip")
+			.help("The IP to connect to.") // Not sure if this is how we're going to do this, just a clap placeholder.
+			.required(false) // Don't make argument required.
+			.index(1))
+		.get_matches();
 
-    // Define variables.
-    // Open log file.
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("templog.txt")
-        .unwrap();
+	// Define variables.
+	// Open log file.
+	let mut file = OpenOptions::new()
+		.append(true)
+		.create(true)
+		.open("templog.txt")
+		.unwrap();
 
-    if let Some(ip) = matches.value_of("ip") { // If IP argument exists
-        // Assume they want to connect to another instance. [Client]
-        let user_id = 0; // Client ID is always 0.
-        let addrs = [
-            SocketAddr::from(([0, 0, 0, 0], 2580)),
-            SocketAddr::from(([0, 0, 0, 0], 2037)),
-        ];
-            if let Ok(_stream) = TcpStream::connect(&addrs[..]) {
-                println!("Connected!");
-            } else {
-                println!("Couldn't connect.");
-            }
+	if let Some(ip) = matches.value_of("ip") { // If IP argument exists
+		// Assume they want to connect to another instance. [Client]
+		let user_id = 0; // Client ID is always 0.
+		let addrs = [
+			SocketAddr::from(([0, 0, 0, 0], 2580)),
+			SocketAddr::from(([0, 0, 0, 0], 2037)),
+		];
+			if let Ok(_stream) = TcpStream::connect(&addrs[..]) {
+				println!("Connected!");
+			} else {
+				println!("Couldn't connect.");
+			}
 
-        let stdin = io::stdin();
-        for line in stdin.lock().lines() {
-            let line = match line {
-                Ok(line) => {
-                    let message = Message {
-                        user_id: user_id.to_owned(),
-                        message: line,
-                    };
-                    let msg = serde_json::to_string(&message)?;
+		let stdin = io::stdin();
+		for line in stdin.lock().lines() {
+			let line = match line {
+				Ok(line) => {
+					let message = Message {
+						user_id: user_id.to_owned(),
+						message: line,
+					};
+					let msg = serde_json::to_string(&message)?;
 
-                    println!("{}", msg);
-                },
-                Err(err) => {
-                    eprintln!("I/O error: {}", err);
-                    continue;
-                },
-            };
-        }
-    } else { // No IP was supplied. Assuming they want to recieve a connection. [Server]
-        let user_id = 1; // Server ID is always 1.
-        // Create a TcpListener.
-        // Use port 2037 if port 2580 fails.
-        let addrs = [
-            SocketAddr::from(([0, 0, 0, 0], 2580)),
-            SocketAddr::from(([0, 0, 0, 0], 2037)),
-        ];
-        let listener = TcpListener::bind(&addrs[..]).unwrap();
+					println!("{}", msg);
+				},
+				Err(err) => {
+					eprintln!("I/O error: {}", err);
+					continue;
+				},
+			};
+		}
+	} else { // No IP was supplied. Assuming they want to recieve a connection. [Server]
+		let user_id = 1; // Server ID is always 1.
+		// Create a TcpListener.
+		// Use port 2037 if port 2580 fails.
+		let addrs = [
+			SocketAddr::from(([0, 0, 0, 0], 2580)),
+			SocketAddr::from(([0, 0, 0, 0], 2037)),
+		];
+		let listener = TcpListener::bind(&addrs[..]).unwrap();
 
-        // Create a builder.
-        // let (acceptor, hash) = AcceptorBuilder::default().build().unwrap();
-        // let (client, _) = listener.accept().unwrap();
-        // let mut client = acceptor.accept(client).unwrap();
+		// Create a builder.
+		// let (acceptor, hash) = AcceptorBuilder::default().build().unwrap();
+		// let (client, _) = listener.accept().unwrap();
+		// let mut client = acceptor.accept(client).unwrap();
 
-        // Accept connections.
-        let mut logfile = file.try_clone()?;
-        thread::spawn(move || {
-            for stream in listener.incoming() {
-                let mut file = match file.try_clone() {
-                    Ok(file) => file,
-                    Err(err) => {
-                        eprintln!("{}", err);
-                        return;
-                    }
-                };
-                // Create a new thread for every client.
-                thread::spawn(move || {
-                    if let Err(err) = stream.and_then(|stream| handle_client(&mut file, user_id, stream)) {
-                        eprintln!("{}", err);
-                    }
-                });
-            }
-        });
+		// Accept connections.
+		let mut logfile = file.try_clone()?;
+		thread::spawn(move || {
+			for stream in listener.incoming() {
+				let mut file = match file.try_clone() {
+					Ok(file) => file,
+					Err(err) => {
+						eprintln!("{}", err);
+						return;
+					}
+				};
+				// Create a new thread for every client.
+				thread::spawn(move || {
+					if let Err(err) = stream.and_then(|stream| handle_client(&mut file, user_id, stream)) {
+						eprintln!("{}", err);
+					}
+				});
+			}
+		});
 
-        // Rustyline.
-        let mut rl = Editor::<()>::new();
-        loop {
-            let readline = rl.readline("> ");
-        match readline {
-            Ok(line) => {
-                log(&mut logfile, user_id, &line);
-                println!("Line: {}", line);
-            },
-            Err(ReadlineError::Interrupted) => {
-                println!("Exiting (Ctrl-C)");
-                break
-            },
-            Err(ReadlineError::Eof) => {
-                println!("Exiting (Ctrl-D)");
-                break
-            },
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break
-            }
-        }}
-    }
+		// Rustyline.
+		let mut rl = Editor::<()>::new();
+		loop {
+			let readline = rl.readline("> ");
+		match readline {
+			Ok(line) => {
+				log(&mut logfile, user_id, &line);
+				println!("Line: {}", line);
+			},
+			Err(ReadlineError::Interrupted) => {
+				println!("Exiting (Ctrl-C)");
+				break
+			},
+			Err(ReadlineError::Eof) => {
+				println!("Exiting (Ctrl-D)");
+				break
+			},
+			Err(err) => {
+				println!("Error: {:?}", err);
+				break
+			}
+		}}
+	}
 
-    // Everything completed without any fatal issues! Well done, code!
-    Ok(())
+	// Everything completed without any fatal issues! Well done, code!
+	Ok(())
 }
 
 // Handle incoming TCP connections.
 fn handle_client(logfile: &mut File, user_id: i8, stream: TcpStream) -> Result<(), Error> {
-    let mut logfile = logfile.try_clone()?;
-    let breader = BufReader::new(stream);
-    for line in breader.lines() {
-        let line = line?;
-        println!("{}", line);
-    }
-    println!("Wow, I got something!");
-    log(&mut logfile, user_id, "Connection was made!");
-    Ok(())
+	let mut logfile = logfile.try_clone()?;
+	let breader = BufReader::new(stream);
+	for line in breader.lines() {
+		let line = line?;
+		println!("{}", line);
+	}
+	println!("Wow, I got something!");
+	log(&mut logfile, user_id, "Connection was made!");
+	Ok(())
 }
 
 // Logging function that logs messages, warnings and errors.
 fn log(logfile: &mut File, id: i8, message: &str) {
-    if let Err(e) = writeln!(logfile, "{},{}", id, message) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
+	if let Err(e) = writeln!(logfile, "{},{}", id, message) {
+		eprintln!("Couldn't write to file: {}", e);
+	}
 }
